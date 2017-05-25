1df13d5bc72769d8ad62d88c2e3818181f46ae74
Saptarshi Roy Choudhuri <saptarshi.roychoudhuri@gmail.com>
2017-05-25 15:14:25 +0530

    First commit

---
+++ app/src/main/java/saptarshi/com/summertask_2/MainActivity.java
@@ -0,0 +1,28 @@
+package saptarshi.com.summertask_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String url="https://qnaxapi.herokuapp.com/memes";
+    ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView image=(ImageView)findViewById(R.id.main_image_view);
+        Picasso.with(this).load(url).into(image);
+    }
+    public void reload(View v){
+        Picasso picasso = Picasso.with(this);
+        picasso.invalidate(url);
+        image=(ImageView)findViewById(R.id.main_image_view);
+        picasso.load(url).into(image);
+    }
+}
